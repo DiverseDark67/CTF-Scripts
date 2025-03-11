4b9880d23150b1819d2a6046355e40f3bb86723b
DiverseDark67 <andrewrhode00@gmail.com>
2025-03-11 13:24:51 -0500

    Added new files

---
+++ Cryptography/Steganography/stepic.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Version 1.0.0
+# Date: 2025-03-11
+
+#      _                _                           _
+#     | |              | |                         (_)                                _____
+#   __| |  ___    _____| |       ___  ___       __  _  _____    ___    _____ ____    /  ___\
+#  / _` | / _ \  /   _/| |___   / _ \ \  \  _  /  /| | | __ \  / _ \  /  __/|  _ \  _| |_    
+# | (_| || |_| | \  \  |  __ \ | |_| | |  \/ \/  | | | | |_| || |_| | \  \  | |_| |[_   _]
+#  \__,_| \___/ |____/ |_,| |_| \___/   \___/\__/  |_| | ___/  \___/ |____/ | ___/   | |
+#                                                      | |                  | |      |_|
+#                                                      |_|                  |_|
+
+from stepic import decode
+from PIL import Image
+
+img_path = input("Enter the path of the image: ")
+img = Image.open(img_path)
+
+hidden_message = decode(img)
+print(hidden_message)
